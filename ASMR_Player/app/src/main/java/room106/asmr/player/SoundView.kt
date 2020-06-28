9d797fdc6956fb1ab72da2a8d802f62709c867f0
chervonyi <yuri.ternopil@gmail.com>
2020-06-27 20:55:17 -0500

    Add animation to control panel

--- ASMR_Player/app/src/main/java/room106/asmr/player/SoundView.kt
+++ ASMR_Player/app/src/main/java/room106/asmr/player/SoundView.kt
@@ -1,19 +1,24 @@
 package room106.asmr.player
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.ValueAnimator
 import android.content.Context
 import android.view.View
 import android.widget.ImageButton
 import android.widget.LinearLayout
 import android.widget.TextView
 
+
 class SoundView(context: Context?, title: String) : LinearLayout(context) {
 
     // Views
-    private lateinit var mIconView: ImageButton
-    private lateinit var mTitleView: TextView
-    private lateinit var mControlPanel: LinearLayout
+    private var mIconView: ImageButton
+    private var mTitleView: TextView
+    private var mControlPanel: LinearLayout
 
     private var controlPanelIsVisible = false
+    private var controlPanelMeasuredHeight: Int
 
     init {
         View.inflate(context, R.layout.sound_layout, this)
@@ -21,19 +26,54 @@ class SoundView(context: Context?, title: String) : LinearLayout(context) {
         mIconView = findViewById(R.id.soundIcon)
         mTitleView = findViewById(R.id.soundTitle)
         mControlPanel = findViewById(R.id.controlPanel)
-        mControlPanel.visibility = View.GONE
+        mControlPanel.visibility = View.INVISIBLE
+
+        mControlPanel.measure(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT)
+        controlPanelMeasuredHeight = mControlPanel.measuredHeight
 
         mTitleView.text = title
 
         mIconView.setOnClickListener {
             controlPanelIsVisible = !controlPanelIsVisible
 
+
             if (controlPanelIsVisible) {
                 // Show panel
                 mControlPanel.visibility = View.VISIBLE
+
+                val valueAnimator =
+                    ValueAnimator.ofInt(0, controlPanelMeasuredHeight)
+
+                valueAnimator.addUpdateListener {
+                    val animatedValue = valueAnimator.animatedValue as Int
+                    val layoutParams = mControlPanel.layoutParams
+                    layoutParams.height = animatedValue
+                    mControlPanel.layoutParams = layoutParams
+                }
+
+                valueAnimator.duration = 300
+                valueAnimator.start()
+
             } else {
                 // Hide panel
-                mControlPanel.visibility = View.GONE
+                val valueAnimator =
+                    ValueAnimator.ofInt(controlPanelMeasuredHeight, 0)
+
+                valueAnimator.addUpdateListener {
+                    val animatedValue = valueAnimator.animatedValue as Int
+                    val layoutParams = mControlPanel.layoutParams
+                    layoutParams.height = animatedValue
+                    mControlPanel.layoutParams = layoutParams
+                }
+
+                valueAnimator.addListener(object : AnimatorListenerAdapter() {
+                    override fun onAnimationEnd(animation: Animator) {
+                        mControlPanel.visibility = View.INVISIBLE
+                    }
+                })
+
+                valueAnimator.duration = 300
+                valueAnimator.start()
             }
         }
     }
