e825ecc446a33b3143bf1628f978a9f7d678ea5f
chervonyi <yuri.ternopil@gmail.com>
2020-07-02 18:20:10 -0500

    Import all sounds

--- ASMR_Player/app/src/main/java/room106/asmr/player/MainActivity.kt
+++ ASMR_Player/app/src/main/java/room106/asmr/player/MainActivity.kt
@@ -10,35 +10,70 @@ import android.widget.LinearLayout
 class MainActivity : AppCompatActivity() {
 
     // Views
-    private lateinit var listNatureSound: LinearLayout
-    private lateinit var listAsrmSound: LinearLayout
+    private lateinit var naturalSoundsList: LinearLayout
+    private lateinit var processSoundsList: LinearLayout
+    private lateinit var asmrSoundsList: LinearLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        listNatureSound = findViewById(R.id.natureSoundList)
-        listAsrmSound = findViewById(R.id.asmrSoundList)
+        naturalSoundsList = findViewById(R.id.naturalSoundsList)
+        processSoundsList = findViewById(R.id.processSoundsList)
+        asmrSoundsList = findViewById(R.id.asmrSoundsList)
 
-        val s1 = SoundView(this, "Ocean with Fade", true, R.raw.ocean_30s_fade)
-        val s2 = SoundView(this, "Ocean", true, R.raw.ocean_30s)
-        val s3 = SoundView(this, "Test", true, R.raw.test_looping_sound)
-        val s4 = SoundView(this, "Test 3s", true, R.raw.test_looping_sound_3s)
+        val s1 = SoundView(this, "Fireplace", true, R.raw.fireplace)
+        val s2 = SoundView(this, "Rain", true, R.raw.rain)
+        val s3 = SoundView(this, "Sea", true, R.raw.sea)
+        val s4 = SoundView(this, "Train", true, R.raw.train)
+        val s5 = SoundView(this, "Water Drops", true, R.raw.water_drops)
+        val s6 = SoundView(this, "Wind", true, R.raw.wind)
 
-        val s5 = SoundView(this, "Wind", true, R.raw.thunderstorm_30s)
-        val s6 = SoundView(this, "Rain", false, R.raw.thunderstorm_30s)
-        val s7 = SoundView(this, "Fire", false, R.raw.thunderstorm_30s)
-        val s8 = SoundView(this, "Sea", false, R.raw.thunderstorm_30s)
+        val s7 = SoundView(this, "Blowing", true, R.raw.blowing)
+        val s8 = SoundView(this, "Cutting", true, R.raw.cutting)
+        val s9 = SoundView(this, "Eating", true, R.raw.eating)
+        val s10 = SoundView(this, "Head Scratching", true, R.raw.head_scratching)
+        val s11 = SoundView(this, "Typing", true, R.raw.typing)
 
-        listNatureSound.addView(s1)
-        listNatureSound.addView(s2)
-        listNatureSound.addView(s3)
-        listNatureSound.addView(s4)
+        val s12 = SoundView(this, "Bottle", true, R.raw.bottle)
+        val s13 = SoundView(this, "Gloves", true, R.raw.gloves)
+        val s14 = SoundView(this, "Hands #1", true, R.raw.hands_1)
+        val s15 = SoundView(this, "Hands #2", true, R.raw.hands_2)
+        val s16 = SoundView(this, "Hands #3", true, R.raw.hands_3)
+        val s17 = SoundView(this, "Massage", true, R.raw.massage)
+        val s18 = SoundView(this, "Mouth", true, R.raw.mouth)
+        val s19 = SoundView(this, "Packaging", true, R.raw.packaging)
+        val s20 = SoundView(this, "Tapping #1", true, R.raw.tapping_1)
+        val s21 = SoundView(this, "Tapping #2", true, R.raw.tapping_2)
+        val s22 = SoundView(this, "Tapping #3", true, R.raw.tapping_3)
+        val s23 = SoundView(this, "Water", true, R.raw.water)
 
-        listAsrmSound.addView(s5)
-        listAsrmSound.addView(s6)
-        listAsrmSound.addView(s7)
-        listAsrmSound.addView(s8)
+
+        naturalSoundsList.addView(s1)
+        naturalSoundsList.addView(s2)
+        naturalSoundsList.addView(s3)
+        naturalSoundsList.addView(s4)
+        naturalSoundsList.addView(s5)
+        naturalSoundsList.addView(s6)
+
+        processSoundsList.addView(s7)
+        processSoundsList.addView(s8)
+        processSoundsList.addView(s9)
+        processSoundsList.addView(s10)
+        processSoundsList.addView(s11)
+
+        asmrSoundsList.addView(s12)
+        asmrSoundsList.addView(s13)
+        asmrSoundsList.addView(s14)
+        asmrSoundsList.addView(s15)
+        asmrSoundsList.addView(s16)
+        asmrSoundsList.addView(s17)
+        asmrSoundsList.addView(s18)
+        asmrSoundsList.addView(s19)
+        asmrSoundsList.addView(s20)
+        asmrSoundsList.addView(s21)
+        asmrSoundsList.addView(s22)
+        asmrSoundsList.addView(s23)
 
     }
 
