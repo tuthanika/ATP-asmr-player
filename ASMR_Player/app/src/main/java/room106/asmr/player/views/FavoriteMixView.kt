16f47beb970914e84d0fa5c81ab45a39c363c83d
tuthanika <tuthanika@gmail.com>
2025-06-22 18:30:55 +0700

    Update FavoriteMixView.kt

--- ASMR_Player/app/src/main/java/room106/asmr/player/views/FavoriteMixView.kt
+++ ASMR_Player/app/src/main/java/room106/asmr/player/views/FavoriteMixView.kt
@@ -1,8 +1,8 @@
 package room106.asmr.player.views
 
+import android.app.AlertDialog
 import android.content.Context
-import android.util.AttributeSet
-import android.view.View
+import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.LinearLayout
 import android.widget.TextView
@@ -10,41 +10,49 @@ import room106.asmr.player.ASMR
 import room106.asmr.player.R
 import room106.asmr.player.activities.FavoritesActivity
 
-class FavoriteMixView: LinearLayout {
+class FavoriteMixView(context: Context, val mixID: Int) : LinearLayout(context) {
+    private val mMixTitleTextView: TextView
+    private val mPlayButton: ImageButton
+    private val mDeleteButton: ImageButton
+    private val mRenameButton: ImageButton
 
-    //region Constructors
-    constructor(context: Context?) : super(context)
-    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
-    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(
-        context,
-        attrs,
-        defStyleAttr
-    )
-    //endregion
-
-    // Views
-    private lateinit var mMixTitleTextView: TextView
-    private lateinit var mPlayButton: ImageButton
-
-
-    private var mixID = -1
-
-    constructor(context: Context?, mixID: Int): super(context) {
-        View.inflate(context,
-            R.layout.favorite_mix_layout, this)
-
-        // Connect views
+    init {
+        View.inflate(context, R.layout.favorite_mix_layout, this)
         mMixTitleTextView = findViewById(R.id.mixTitleTextView)
         mPlayButton = findViewById(R.id.playMixButton)
+        mDeleteButton = findViewById(R.id.buttonDeleteMix)
+        mRenameButton = findViewById(R.id.buttonRenameMix)
 
-        this.mixID = mixID
+        mMixTitleTextView.text = ASMR.player.mixesList.getMixName(mixID)
 
-        mMixTitleTextView.text = resources.getString(R.string.mix_title_attr, mixID + 1)
         mPlayButton.setOnClickListener {
             ASMR.player.playMix(mixID)
-
-            (context as FavoritesActivity).updateMixesIcons()
-            context.checkSaveCurrentMixButton()
+            (context as? FavoritesActivity)?.updateMixesIcons()
+            (context as? FavoritesActivity)?.checkSaveCurrentMixButton()
+        }
+        mDeleteButton.setOnClickListener {
+            AlertDialog.Builder(context)
+                .setTitle("Xoá mix")
+                .setMessage("B?n mu?n xoá mix này?")
+                .setPositiveButton("Xoá") { _, _ ->
+                    ASMR.player.mixesList.removeAt(context, mixID)
+                    (context as? FavoritesActivity)?.updateMixesListView()
+                }
+                .setNegativeButton("Hu?", null)
+                .show()
+        }
+        mRenameButton.setOnClickListener {
+            val editText = EditText(context)
+            AlertDialog.Builder(context)
+                .setTitle("Ð?i tên mix")
+                .setView(editText)
+                .setPositiveButton("Luu") { _, _ ->
+                    ASMR.player.mixesList.renameMix(context, mixID, editText.text.toString())
+                    mMixTitleTextView.text = editText.text.toString()
+                    (context as? FavoritesActivity)?.updateMixesListView()
+                }
+                .setNegativeButton("Hu?", null)
+                .show()
         }
     }
 
